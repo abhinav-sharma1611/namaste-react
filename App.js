a987efb3ebb11940e2af4b64b5dca2b3b4ff0eff
Apple <apple@Apples-MacBook-Air.local>
2024-10-01 21:00:04 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+
+const heading = React.createElement("div",
+  {
+    id: "parent"
+  },
+  React.createElement("div",
+    {
+      id: "child"
+    },
+    React.createElement("h1",
+      {
+
+      }, "I am a h1 tag"
+    )
+  )
+);
+
+console.log(heading, "heading")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
