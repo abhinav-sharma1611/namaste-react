e76a454e7d634b6440d6597d2be9eac357cad0f7
Apple <apple@Apples-MacBook-Air.local>
2024-10-01 22:11:15 +0530

    Episode 1

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
-
-const heading = React.createElement("div",
-  {
-    id: "parent"
-  },
-  React.createElement("div",
-    {
-      id: "child"
-    },
-    React.createElement("h1",
-      {
-
-      }, "I am a h1 tag"
-    )
-  )
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ])
 );
 
-console.log(heading, "heading")
+console.log(heading, "heading");
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
+root.render(heading);
